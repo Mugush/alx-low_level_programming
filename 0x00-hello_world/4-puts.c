21431f1894e75c3e62cb211be7fc23222f2cbcc7
Mugush <mugurekanja2020@gmail.com>
2023-02-16 12:17:35 -0800

    my first code of c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main (void) 
+/**
+ *main - print a file using puts
+ *
+ *Return:0
+ */
+int main(void)
 {
-	puts ("Programming is like building a multilingual puzzle\n");
-		return 0;
+	puts("\"Programming is like building a multilingual puzzle\n");
+		return (0);
 }
