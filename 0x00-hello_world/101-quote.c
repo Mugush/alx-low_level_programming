0933bfbebdf793c31ba9e526f80d01fc3524d5d6
Mugush <mugurekanja2020@gmail.com>
2023-02-17 10:17:47 -0800

    Task 8 unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <unistd.h>
 /**
- *Task 8.UNIX solution
+ *main - Task 8.UNIX solution
  *
+ *Return:1.
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful'' - Dora Korpar, 2015-10-19\n", 61);
+	write(2, and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
 	return (1);
 }
