e0189dc0edf33f041addb6d6b59f38bb764fa5e2
Mugush <mugurekanja2020@gmail.com>
2023-02-17 13:48:42 -0800

    Betty style

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
- *main - Task 8.UNIX solution
- *
- *Return:1.
+ * main - Betty style
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 61);
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
