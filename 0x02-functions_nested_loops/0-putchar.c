0b6c5bdcab65b9a4a2e4c75b7581a3a60e180323
Mugush <mugurekanja2020@gmail.com>
2023-02-21 11:59:13 -0800

    print _Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,19 @@
-#include <main.h>
+#include "main.h"
 /**
  * main -prints _putchar
  * Date:21/02/2023
  * Return : Always 0 (success)
  */
 int main(void)
-
-	int _putchar("\n");
 {
+	 _putchar('_');
+	 _putchar('p');
+	 _putchar('u');
+	 _putchar('t');
+	 _putchar('c');
+	 _putchar('h');
+	 _putchar('a');
+	 _putchar('r');
+	 _putchar('\n');
 	return(0);
 }
