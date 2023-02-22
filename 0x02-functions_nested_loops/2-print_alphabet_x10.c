8983e4e74627bb368026a0e2c8a4486a3a8d5e92
Mugush <mugurekanja2020@gmail.com>
2023-02-22 07:56:18 -0800

    prints 10 times the alphabet, in lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
 /**
  * print_alphabet_x10 in lower case
  *
- * Main:lower case 10x alphabets
+ * Description:lower case 10x alphabets
  * Return always 0 (success)
  */
 void print_alphabet_x10(void)
@@ -13,9 +13,9 @@ void print_alphabet_x10(void)
 
 	for (t = 1 ; t <= 10; t++)
 	{
-		for (ch = 'a' ;ch <= 'z'; ch++)
+		for (ch = 'a' ; ch <= 'z'; ch++)
 			_putchar(t);
-		_putchar('\n');
+				_putchar('\n');
 	}
 }
 
