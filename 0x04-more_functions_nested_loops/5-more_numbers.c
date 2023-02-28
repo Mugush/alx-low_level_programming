2b568c6bf3cb8cab519bf6bf3f6ef806949e6f31
Mugush <mugurekanja2020@gmail.com>
2023-02-27 19:07:57 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * more_numbers - prints 10 times the numbers, from 0 to 14
- * followed by a new line
+ * Return: 10 timesof the numbers from 0 up to 14
  */
 void more_numbers(void)
 {
@@ -10,11 +10,12 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-	for (j = 0; j < 15; j++)
+	for (j = 0; j <= 14; j++)
 	{
-	if (j >= 10)
-	{	_putchar(j / 10 + '0');
-		_putchar(j % 10 + '0');
+	if (j > 9)
+	{	_putchar((j / 10 + '0');
+	}
+	{	_putchar((j % 10 + '0');
 	}
 		_putchar('\n');
 	}
