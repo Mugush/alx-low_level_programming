c9263c33c4b60f4ad6330393bb1dbf6010725422
Mugush <mugurekanja2020@gmail.com>
2023-02-28 09:33:32 -0800

    100-times_tables.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -19,37 +19,37 @@ void print_times_table(int n)
 		{
 		for (column = 0; column <= n; column++)
 		{
-				int product = column * row;
+		int product = column * row;
 
-				if (column == 0)
-				{
-					putchar('0');
-				}
-				else if (product <= 9)
-				{
-					putchar(',');
-					putchar(' ');
-					putchar(' ');
-					putchar(' ');
-					putchar('0' + product);
-				}
-				else if (product > 99)
-					{
-					putchar(',');
-					putchar(' ');
-					putchar('0' + (product / 100));
-					putchar('0' + ((product / 10) % 10));
-					putchar('0' + (product % 10));
-				}
-				else
-				{
-					putchar(',');
-					putchar(' ');
-					putchar(' ');
-					putchar('0' + (product / 10));
-					putchar('0' + (product % 10));
-				}
-			}
+		if (column == 0)
+		{
+			putchar('0');
+		}
+		else if (product <= 9)
+		{
+				putchar(',');
+				putchar(' ');
+				putchar(' ');
+				putchar(' ');
+				putchar('0' + product);
+		}
+		else if (product > 99)
+		{
+				putchar(',');
+				putchar(' ');
+				putchar('0' + (product / 100));
+				putchar('0' + ((product / 10) % 10));
+				putchar('0' + (product % 10));
+		}
+		else
+		{
+				putchar(',');
+				putchar(' ');
+				putchar(' ');
+				putchar('0' + (product / 10));
+				putchar('0' + (product % 10));
+		}
+		}
 			putchar('\n');
 		}
 	}
