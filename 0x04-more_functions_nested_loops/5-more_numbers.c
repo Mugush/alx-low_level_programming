d82f6160e204860c09f61acd5538576ec214a970
Mugush <mugurekanja2020@gmail.com>
2023-03-04 21:16:33 -0800

    5-mire_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
- * more_numbers - prints 10 times the numbers, from 0 to 14
- * Return: 10 timesof the numbers from 0 up to 14
+ * print_most_numbers - checks for checks for a digit (0 through 9).
+ *
+ * Return: Always 0.
  */
-void more_numbers(void)
+void print_most_numbers(void)
 {
-	int i, j;
+int c;
 
-	for (i = 0; i < 10; i++)
-	{
-	for (j = 0; j <= 14; j++)
-	{
-	if (j > 9)
-	{	_putchar((j / 10 + '0');
-	}
-	{	_putchar((j % 10 + '0');
-	}
-		_putchar('\n');
-	}
-	}
+for (c = 48; c < 58; c++)
+{
+if (c != 50)
+{
+if (c != 52)
+{
+_putchar(c);
+}
+}
+}
+_putchar('\n');
 }
