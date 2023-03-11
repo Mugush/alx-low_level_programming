ec8729beea8584976834b786d02df702e91997c8
Mugush <mugurekanja2020@gmail.com>
2023-03-10 17:11:50 -0800

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,14 +2,17 @@
 
 /**
  * main- prints all arguments it receives.
- * @argc: array size.
- * @argv: array containing arguments.
+ * @argc: argument count.
+ * @argv: arguments value
  *
  * Return: 0 on success
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	printf("argc=%d\n", argc);
+	while (argc--)
+	{
+		printf("%s\n", *argv++);
+	}
 
 	return (0);
 }
