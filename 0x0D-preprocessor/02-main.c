5d1d449655c71f6a154e63e4cf47c25734ffe342
Mugush <mugurekanja2020@gmail.com>
2023-03-20 12:17:16 -0700

    2-main.c

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,1 +1,13 @@
-Happy coding
+#include <stdio.h>
+
+/**
+ * main- prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Name of source file: %s\n", __FILE__);
+
+	return (0);
+}
