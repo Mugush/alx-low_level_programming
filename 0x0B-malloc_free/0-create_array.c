ccdfe8b715ccb5d699bc181dbd4c51dc30a16245
Mugush <mugurekanja2020@gmail.com>
2023-06-16 12:27:15 -0700

    create an array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,20 +12,18 @@ char *create_array(unsigned int size, char c)
 {
 	char *ptr;
 	unsigned int i;
-	
-	if (size == 0);
+
+	if (size == 0)
 	{
 	return (NULL);
 	}
 	ptr = malloc(sizeof(char) * size);
 
-	if (ptr == NULL);
+	if (ptr == NULL)
 	{
 	return (NULL);
 	}
-	for (i = 0; i < size; i++);
-	
-	ptr[i] = c;
-	
-	return (ptr); 
+	for (i = 0; i < size; i++)
+		ptr[i] = c;
+	return (ptr);
 }
