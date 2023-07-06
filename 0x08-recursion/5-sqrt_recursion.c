c53b095b0819e939351dd8397d5778cbc6f27494
Mugush <mugush2020@gmail.com>
2023-07-05 17:18:03 -0700

    square-root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,36 +1,76 @@
-#include <math.h>
+#include <stdio.h>
 /**
  * _sqrt_recursion -function that returns natural square root of a number.
  * @n:number to calculate the square root of.
  *
  * Return: the result of the square root
  */
-int _sqrt_recursion(int n)
-{
-	if (n < 0)
-	{return -1;
-	}
-	if (n == 0 || n == 1)
-	{return n;
-	}
-	int result = sqrt_helper(n, 1, n);
-	return (result);
-}
-
-int sqrt_helper(int n, int start, int end)
+int sqrt_recursive_helper(int n, int start, int end)
 {
 	if (start > end)
-	{ return -1;
+
+	{
+        return (-1);
 	}
-int mid = start + (end - start) / 2;
-{	
-	if (mid == n / mid)
-	{ return mid;
+
+	int mid = (start + end) / 2;
+
+	if (mid * mid == n) 
+	{
+	return (mid);
+	}		
+	else if (mid * mid < n) 
+    	{
+	return (sqrt_recursive_helper(n, mid + 1, end));
 	}
-	else if (mid < n / mid)
-	{return sqrt_helper(n, mid + 1, end);
+	else 
+	{
+	return (sqrt_recursive_helper(n, start, mid - 1));
 	}
-	else
-	{return sqrt_ helper(n, start, mid - 1);
+}
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0) {
+	return (-1);
 	}
+	if (n == 0 || n == 1) {
+	return (n);
+	}
+	return (sqrt_recursive_helper(n, 1, n));
+}
+
+void _putchar(char c)
+{
+	putc(c, stdout);
+}
+
+void print_number(int num)
+{
+	if (num > 9) 
+    	{	
+        print_number(num / 10);
+	}
+
+	_putchar((num % 10) + '0');
+}
+
+void print_sqrt(int n)
+{
+	if (n < 0) 
+	{
+	_putchar('-');
+	_putchar('1');
+	}
+	else 
+	{
+	int sqrt = _sqrt_recursion(n);
+	print_number(sqrt);
+	}
+	_putchar('\n');
+}
+int main(void)
+{
+	print_sqrt(16); 
+		return (0);
 }
