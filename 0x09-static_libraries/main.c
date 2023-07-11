0b913105d769c132bcf2922b24e5a1914cf26881
Mugush <mugush2020@gmail.com>
2023-07-11 10:12:52 -0700

    static debugging

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,11 @@
 #include "main.h"
+/**
+ * Description - Goal was to be the best hacker.
+ *
+ */
 
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	return (0);
 }
