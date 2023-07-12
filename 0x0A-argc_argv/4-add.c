0aab6fa2e9697ce1c180025aa2f040f48230610f
Mugush <mugush2020@gmail.com>
2023-07-12 09:14:58 -0700

    a program that adds positive numbers.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,38 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
-* main- A program that add 2positive numbers, followed by new line.
-*@argc: the argument count
-*@argv: the argument vector
-*Return: 0 failure, 1 if one number contains symbols taht are not digits.
-*/
+ * main - A program that adds positive numbers.
+ * @argc: The argument count
+ * @argv: The argument vector
+ * Return: 0 on success, 1 if one number contains symbols that are not digits.
+ */
 int main(int argc, char *argv[])
 {
 	int i, num, sum = 0;
 
 	if (argc == 1)
 	{
-	printf("0\n");
-	return (0);
+		printf("0\n");
+		return (0);
 	}
 
 	for (i = 1; i < argc; i++)
 	{
-	int j = 0;
+		char *ptr = argv[i];
 
-	while (argv[i][j] != '\0')
-	{
-		if (argv[i][j] < '0' || argv[i][j] > '9')
+		while (*ptr != '\0')
 		{
-		printf("Error\n");
-		return (1);
-	}
-	j++;
+			if (*ptr < '0' || *ptr > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			ptr++;
+		}
+
+		num = atoi(argv[i]);
+		sum += num;
 	}
 
-	num = atoi(argv[i]);
-	sum += num;
-
-	printf("%d\n", result);
+	printf("%d\n", sum);
 	return (0);
 }
