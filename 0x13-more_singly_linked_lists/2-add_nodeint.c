ed84a6412214bc53726f6447c1db36b2219ed071
Mugush <mugurekanja2020@gmail.com>
2023-08-01 14:00:58 -0700

    a function that adds a new node at the beginning of a listint_t list.

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,4 +1,5 @@
 #include <stddef.h>
+#include "lists.h"
 
 /**
  * listint_len - Returns the number of elements in a linked listint_t list.
@@ -17,5 +18,5 @@ size_t listint_len(const listint_t *h)
 		node_count++;
 	}
 
-	return node_count;
+	return (node_count);
 }
